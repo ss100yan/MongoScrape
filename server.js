3d613e1bd32eb024ba0fcf42e2d246fca3958911
ss100yan <32798618+ss100yan@users.noreply.github.com>
2019-04-20 21:54:18 -0400

    new

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.set('view engine', 'handlebars');
 
 //connecting to MongoDB
 
-var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://ss100yan:superstoy1@ds349455.mlab.com:49455/heroku_bwcnzgcm';
+// var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://ss100yan:superstoy1@ds349455.mlab.com:49455/heroku_bwcnzgcm';
 
-mongoose.connect(MONGODB_URI);
+// mongoose.connect(MONGODB_URI);
 
 // mongoose.connect('mongodb://ss100yan:superstoy1@ds349455.mlab.com:49455/heroku_bwcnzgcm');
   
@@ -33,11 +33,11 @@ mongoose.connect(MONGODB_URI);
 
 
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Connected to Mongoose!')
-});
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   console.log('Connected to Mongoose!')
+// });
 
 var routes = require('./controller/controller.js');
 app.use('/', routes);
