5024e7fe6a7a6ce7f383119fe51d4980cb4a40a7
ss100yan <32798618+ss100yan@users.noreply.github.com>
2019-04-22 11:56:56 -0400

    new

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
-//dependencies
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-
-
-//initialize Express app
-var express = require('express');
-var app = express();
-
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
 
 app.use(bodyParser.urlencoded({
   extended: false
@@ -14,32 +10,29 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static(process.cwd() + '/public'));
 
-var exphbs = require('express-handlebars');
+
 
 app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
 
-//connecting to MongoDB
+
 
 var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://ss100yan:superstoy1@ds349455.mlab.com:49455/heroku_bwcnzgcm';
 
 mongoose.connect(MONGODB_URI);
-
-// mongoose.connect('mongodb://ss100yan:superstoy1@ds349455.mlab.com:49455/heroku_bwcnzgcm');
   
-
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log('Connected to Mongoose!')
 });
 
-var routes = require('./controller/controller.js');
+const routes = require('./controller/controller.js');
 app.use('/', routes);
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, function(){
   console.log('Listening on PORT ' + port);
